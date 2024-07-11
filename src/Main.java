e3c0e9725fc7bcedba82a4882433591339dc0cab
여경현 <121675605+yeokyunghyun@users.noreply.github.com>
2024-07-11 16:49:51 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * packageName    :
+ * fileName       :
+ * author         : njy
+ * date           : 2024-07-11
+ * description    : <<여기 설명>>
+ * <pre>
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-07-11           njy            최초 생성
+ * </pre>
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
